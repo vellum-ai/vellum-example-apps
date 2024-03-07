2553f6e221eb4447afb46acaacc8c41a9a4cfd6b
David Vargas <dvargas92495@gmail.com>
2024-03-07 14:15:54 -0500

    add back auth

--- examples/vercel-ai-chatbot/auth.ts
+++ examples/vercel-ai-chatbot/auth.ts
@@ -10,39 +10,30 @@ declare module 'next-auth' {
   }
 }
 
-// Didn't want to setup a Github App
-// export const {
-//   handlers: { GET, POST },
-//   auth
-// } = NextAuth({
-//   providers: [GitHub],
-//   callbacks: {
-//     jwt({ token, profile }) {
-//       if (profile) {
-//         token.id = profile.id
-//         token.image = profile.avatar_url || profile.picture
-//       }
-//       return token
-//     },
-//     session: ({ session, token }) => {
-//       if (session?.user && token?.id) {
-//         session.user.id = String(token.id)
-//       }
-//       return session
-//     },
-//     authorized({ auth }) {
-//       return !!auth?.user // this ensures there is a logged in user for -every- request
-//     }
-//   },
-//   pages: {
-//     signIn: '/sign-in' // overrides the next-auth default signin page https://authjs.dev/guides/basics/pages
-//   }
-// })
-
-export const auth = async () => {
-  return {
-    user: {
-      id: 'vellum'
+export const {
+  handlers: { GET, POST },
+  auth
+} = NextAuth({
+  providers: [GitHub],
+  callbacks: {
+    jwt({ token, profile }) {
+      if (profile) {
+        token.id = profile.id
+        token.image = profile.avatar_url || profile.picture
+      }
+      return token
+    },
+    session: ({ session, token }) => {
+      if (session?.user && token?.id) {
+        session.user.id = String(token.id)
+      }
+      return session
+    },
+    authorized({ auth }) {
+      return !!auth?.user // this ensures there is a logged in user for -every- request
     }
+  },
+  pages: {
+    signIn: '/sign-in' // overrides the next-auth default signin page https://authjs.dev/guides/basics/pages
   }
-}
+})
