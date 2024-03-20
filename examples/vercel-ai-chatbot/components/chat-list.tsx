18f7336607e290df32c095339a7beafeb90e5b10
David Vargas <vargas@vellum.ai>
2024-03-20 08:43:10 -0400

    Adding more responsiveness to the chat (#13)
    * Adding more responsiveness to the chat
    
    * cleanup
    
    * build error

--- examples/vercel-ai-chatbot/components/chat-list.tsx
+++ examples/vercel-ai-chatbot/components/chat-list.tsx
@@ -1,12 +1,16 @@
 import { Separator } from '@/components/ui/separator'
 import { ChatMessageComponent } from '@/components/chat-message-component'
 import { ChatMessage } from 'vellum-ai/api'
+import { useMemo } from 'react'
+import { IconOpenAI } from './ui/icons'
 
 export interface ChatList {
   messages: ChatMessage[]
+  isLoading?: boolean
 }
 
-export function ChatList({ messages }: ChatList) {
+export function ChatList({ messages, isLoading = false }: ChatList) {
+  const lastMessage = useMemo(() => messages.slice(-1)[0], [messages])
   if (!messages.length) {
     return null
   }
@@ -21,6 +25,23 @@ export function ChatList({ messages }: ChatList) {
           )}
         </div>
       ))}
+      {isLoading && ['USER', 'FUNCTION'].includes(lastMessage.role) && (
+        <div>
+          <Separator className="my-4 md:my-8" />
+          <div className={'relative mb-4 flex items-start md:-ml-12'}>
+            <div
+              className={
+                'flex size-8 shrink-0 select-none items-center justify-center rounded-md border shadow'
+              }
+            >
+              <IconOpenAI />
+            </div>
+            <div className="flex-1 px-1 ml-4 space-y-2 overflow-hidden italic">
+              Typing...
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
